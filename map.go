b471f539aa03ebb1bb55db1d71be3e753d8f605e
meowdada <j1597537@gmail.com>
2020-07-23 18:04:42 +0800

    add lock for map

--- map.go
+++ map.go
@@ -1,10 +1,14 @@
 package fcache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // Map implements DB interface.
 type hashmap struct {
-	m map[string]Item
+	m  map[string]Item
+	mu sync.RWMutex
 }
 
 // Hashmap returns a DB instance implemented by a hashmap.
@@ -14,17 +18,81 @@ func Hashmap() DB {
 	}
 }
 
-func (hm *hashmap) Iter(iterCb func(k string, v Item) error) error {
-	for k, v := range hm.m {
-		err := iterCb(k, v)
-		if err != nil {
-			return err
+func (hm *hashmap) Iter(iterCb func(k string, v Item) error) (err error) {
+	hm.rlockFn(func() {
+		err = hm.iter(iterCb)
+	})
+	return err
+}
+
+func (hm *hashmap) Put(key string, size int64) (err error) {
+	hm.lockFn(func() {
+		err = hm.put(key, size)
+	})
+	return err
+}
+
+func (hm *hashmap) Get(key string) (item Item, err error) {
+	hm.rlockFn(func() {
+		item, err = hm.get(key)
+	})
+	return item, err
+}
+
+func (hm *hashmap) Remove(key string) error {
+	hm.lockFn(func() {
+		delete(hm.m, key)
+	})
+	return nil
+}
+
+func (hm *hashmap) IncrRef(keys ...string) (err error) {
+	hm.lockFn(func() {
+		err = hm.incrRef(keys...)
+	})
+	return err
+}
+
+func (hm *hashmap) DecrRef(keys ...string) (err error) {
+	hm.lockFn(func() {
+		err = hm.decrRef(keys...)
+	})
+	return err
+}
+
+func (hm *hashmap) Close() error {
+	return nil
+}
+
+func (hm *hashmap) incrRef(keys ...string) error {
+	for _, key := range keys {
+		item, ok := hm.m[key]
+		if !ok {
+			item = NewDummyItem(key)
+		}
+		item.Ref++
+		item.Used++
+		item.LastUsed = time.Now()
+		hm.m[key] = item
+	}
+	return nil
+}
+
+func (hm *hashmap) decrRef(keys ...string) error {
+	for _, key := range keys {
+		item, ok := hm.m[key]
+		if !ok {
+			continue
+		}
+		if item.Ref > 0 {
+			item.Ref--
+			hm.m[key] = item
 		}
 	}
 	return nil
 }
 
-func (hm *hashmap) Put(key string, size int64) error {
+func (hm *hashmap) put(key string, size int64) error {
 	item, ok := hm.m[key]
 	if !ok {
 		item = NewItem(key, size)
@@ -42,7 +110,7 @@ func (hm *hashmap) Put(key string, size int64) error {
 	return ErrDupKey
 }
 
-func (hm *hashmap) Get(key string) (Item, error) {
+func (hm *hashmap) get(key string) (Item, error) {
 	item, ok := hm.m[key]
 	if ok {
 		return item, nil
@@ -50,39 +118,24 @@ func (hm *hashmap) Get(key string) (Item, error) {
 	return Item{}, ErrCacheMiss
 }
 
-func (hm *hashmap) Remove(key string) error {
-	delete(hm.m, key)
-	return nil
-}
-
-func (hm *hashmap) IncrRef(keys ...string) error {
-	for _, key := range keys {
-		item, ok := hm.m[key]
-		if !ok {
-			item = NewDummyItem(key)
-		}
-		item.Ref++
-		item.Used++
-		item.LastUsed = time.Now()
-		hm.m[key] = item
-	}
-	return nil
-}
-
-func (hm *hashmap) DecrRef(keys ...string) error {
-	for _, key := range keys {
-		item, ok := hm.m[key]
-		if !ok {
-			continue
-		}
-		if item.Ref > 0 {
-			item.Ref--
-			hm.m[key] = item
+func (hm *hashmap) iter(iterCb func(k string, v Item) error) error {
+	for k, v := range hm.m {
+		err := iterCb(k, v)
+		if err != nil {
+			return err
 		}
 	}
 	return nil
 }
 
-func (hm *hashmap) Close() error {
-	return nil
+func (hm *hashmap) lockFn(fn func()) {
+	hm.mu.Lock()
+	fn()
+	hm.mu.Unlock()
+}
+
+func (hm *hashmap) rlockFn(fn func()) {
+	hm.mu.RLock()
+	fn()
+	hm.mu.RUnlock()
 }
