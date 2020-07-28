0b99908874b8bb923c618a5cb0e91c307088cc6e
meowdada <j1597537@gmail.com>
2020-07-28 11:27:07 +0800

    adds comment on policy interface

--- policy/policy.go
+++ policy/policy.go
@@ -3,7 +3,8 @@ package policy
 import "github.com/meowdada/go-fcache/cache"
 
 // Policy is a cache replacement algorithm which able to Evict a cache item
-// from a cache pool.
+// from a cache pool. If it is unable to evict any cache item so far, return
+// a special errors as ErrNoEmitableCaches.
 type Policy interface {
 	Evict(db cache.Pool) (cache.Item, error)
 }
